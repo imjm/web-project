2f3b08dc2ed84fd82758754eb79881456feb5496
imjm <77835002+imjm@users.noreply.github.com>
2022-11-29 21:58:00 +0900

    시작 페이지 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,25 +5,15 @@ import { useNavigate } from 'react-router-dom'
 
 function LandingPage(props) {
 
-    const navigate = useNavigate();
+    
 
-    const onClickHandler = () => {
-      axios.get('/api/users/logout')
-      .then(response => {
-        if(response.data.success) {
-          navigate("/login")
-        } else {
-          alert("로그아웃 하는데 실패했습니다.")
-        }
-      })
-    }
+    
     
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
-      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
