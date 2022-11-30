57a7598db46aa63d56663d71220d217b43707303
imjm <77835002+imjm@users.noreply.github.com>
2022-11-30 22:37:09 +0900

    로그인 페이지 체크포인트

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
-import {useDispatch} from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { loginUser} from '../../../_actions/user_action'
+import { Form, Input, Button, Checkbox, Typography } from 'antd';
+import Icon from '@ant-design/icons';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const { Title } = Typography;
 
 function LoginPage(props) {
 
@@ -42,15 +48,15 @@ function LoginPage(props) {
 
 
   return (
-    <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
-    }}>
+    <div className="app">
+
+      <Title level={2} >로그인</Title>
 
       <form style={{display: 'flex', flexDirection:'column'}} onSubmit={onSubmitHandler}>
         <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
+        <input type="email" value={Email} onChange={onEmailHandler} required/>
         <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler}/>
+        <input type="password" value={Password} onChange={onPasswordHandler} required/>
 
         <br />
         <button>
