56a1d9277723dfafff95ab1556acbacc4f39e0de
imjm <77835002+imjm@users.noreply.github.com>
2022-12-02 19:07:42 +0900

    게시판 구현중

---
+++ server/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: String, default: moment().format("YYYY-MM-DD hh:mm:ss") },
+  updatedAt: { type: Date },
+});
+
+const Post = mongoose.model("post", postSchema);
+module.exports = Post;
